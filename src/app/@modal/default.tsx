6bf0e9b82f6b46d008e5fff8d29f824a3ec7dbab
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-12-07 13:57:11 +0530

    feat: parallel route for contributions
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

---
+++ src/app/@modal/default.tsx
@@ -0,0 +1,6 @@
+// just here for next to render something even if the modal is disabled
+const DefaultModal = () => {
+  return null;
+};
+
+export default DefaultModal;
--- src/app/utils/dummyData.tsx
+++ src/app/utils/dummyData.tsx
@@ -2,28 +2,37 @@ import { Player } from "./types";
 
 export const players: Player[] = [
   {
+    userId: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
     rank: 1,
     player: "marion_stiedemann",
     won: 2.0,
     trades: 5,
     winRate: "34.07%",
     volume: 0.56,
+    image:
+      "https://images.unsplash.com/photo-1733170616326-84000c81fba3?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
+    userId: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
     rank: 2,
     player: "shannon_kautzer",
     won: 2.0,
     trades: 5,
     winRate: "34.07%",
     volume: 0.56,
+    image:
+      "https://images.unsplash.com/photo-1733170616326-84000c81fba3?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
+    userId: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b",
     rank: 3,
     player: "billy_mraz",
     won: 2.0,
     trades: 5,
     winRate: "34.07%",
     volume: 0.56,
+    image:
+      "https://images.unsplash.com/photo-1733170616326-84000c81fba3?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   // ... other players
 ];
