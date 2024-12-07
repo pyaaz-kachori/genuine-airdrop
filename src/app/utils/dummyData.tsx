a5e08ef76750d67119685311371f90490b6168ba
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-12-07 06:25:00 +0530

    feat: basic leaderboard
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

---
+++ src/app/utils/dummyData.tsx
@@ -0,0 +1,29 @@
+import { Player } from "./types";
+
+export const players: Player[] = [
+  {
+    rank: 1,
+    player: "marion_stiedemann",
+    won: 2.0,
+    trades: 5,
+    winRate: "34.07%",
+    volume: 0.56,
+  },
+  {
+    rank: 2,
+    player: "shannon_kautzer",
+    won: 2.0,
+    trades: 5,
+    winRate: "34.07%",
+    volume: 0.56,
+  },
+  {
+    rank: 3,
+    player: "billy_mraz",
+    won: 2.0,
+    trades: 5,
+    winRate: "34.07%",
+    volume: 0.56,
+  },
+  // ... other players
+];
