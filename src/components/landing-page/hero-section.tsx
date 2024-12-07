d675314229a8783f679958f55e7f6d4d2c9b1da9
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-12-07 16:02:02 +0530

    feat: final hero section
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/components/landing-page/hero-section.tsx
+++ src/components/landing-page/hero-section.tsx
@@ -1,17 +1,47 @@
-// "use client";
+"use client";
+
+import HyperText from "../ui/hyper-text";
+
 const HeroSection = () => {
   return (
-    <div className="h-screen border relative">
+    <div className="h-screen font-sans relative flex items-center justify-center text-primary-text">
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="absolute top-0 -z-20 w-full h-full object-cover"
+        className="absolute top-0 left-0 -z-20 w-full h-full object-cover"
       >
         <source src="/videos/hero.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      <div className="text-center px-4">
+        <HyperText
+          className="text-4xl md:text-6xl font-bold mb-4"
+          text="Revolutionizing Crypto Airdrops"
+        />
+
+        <p className="text-lg md:text-xl font-light mb-8">
+          Leverage GitHub insights to create fair and impactful token
+          distributions for genuine contributors.
+        </p>
+        <div className="space-x-4">
+          <button className="px-6 py-3 hover:bg-primary-text hover:text-primary-bg font-semibold border border-primary-text backdrop-blur-sm rounded-full text-lg transition duration-500">
+            Register Your Organization
+          </button>
+          <button className="px-6 py-3 hover:bg-transparent text-primary-bg bg-primary-text hover:text-primary-text font-semibold border border-primary-text backdrop-blur-sm rounded-full text-lg transition duration-500">
+            Learn More
+          </button>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 bg-black/50 px-6 py-3 rounded-lg backdrop-blur-md shadow-lg">
+        <p className="text-sm md:text-base">
+          <span className="font-bold">$20M+</span> Tokens Distributed |{" "}
+          <span className="font-bold">500+</span> Projects Rewarded
+        </p>
+      </div>
     </div>
   );
 };
